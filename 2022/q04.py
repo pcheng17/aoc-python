6b75ba6dbf4e3b7d1f11142ad90ff101f22e75db
Peter Cheng <pcheng17@gmail.com>
2022-12-03 21:26:14 -0800

    Day 4

---
+++ 2022/q04.py
@@ -0,0 +1,36 @@
+from aocd import data
+
+def containsOther(x, y, u, v):
+    return ((x<=u) and (y>=v))
+
+def isRedundant(x, y, u, v):
+    return containsOther(x, y, u, v) or containsOther(u, v, x, y)
+
+def isOverlapping(x, y, u, v):
+    return ((y>=u) and (v>=x))
+
+
+def partA(dataList):
+    result = 0
+    for line in dataList:
+        r1, r2 = line.split(',')
+        r1a, r1b = map(int, r1.split('-'))
+        r2a, r2b = map(int, r2.split('-'))
+        result += int(isRedundant(r1a, r1b, r2a, r2b))
+    return result 
+
+
+def partB(dataList):
+    result = 0
+    for line in dataList:
+        r1, r2 = line.split(',')
+        r1a, r1b = map(int, r1.split('-'))
+        r2a, r2b = map(int, r2.split('-'))
+        if isOverlapping(r1a, r1b, r2a, r2b):
+            result += 1
+    return result 
+
+
+dataList = data.split('\n')
+print(f'Part A: {partA(dataList)}')
+print(f'Part B: {partB(dataList)}')
