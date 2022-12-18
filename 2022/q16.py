0b5a5b9563a0b1f56bcbfa50af433573c508d652
Peter Cheng <pcheng17@gmail.com>
2022-12-17 20:40:04 -0800

    WIP 16

--- 2022/q16.py
+++ 2022/q16.py
@@ -1,24 +1,44 @@
 from aocd import data
 import re
 
+test='''
+Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II
+'''
 
 def parse(data):
-    valveMap = {}
-    flowMap = {}
+    map = {}
     for line in data.strip().split('\n'):
         src, *dst = re.findall('[A-Z][A-Z]', line)
-        valveMap[src] = dst
-        flowMap[src] = int(re.findall('[0-9]+', line)[0])
-    return (valveMap, flowMap)
+        flow = int(re.findall('[0-9]+', line)[0])
+        map[src] = {'goto': dst, 'flow': flow}
+    return map
 
 
-def partA(valveMap, flowMap):
+def partA(input):
 
-    flows = [(k,v) for k, v in flowMap.items()]
-    flows = sorted(flows, key = lambda x : x[1], reverse=True)
+    def dfs(curr, time, seen):
+        if time <= 1:
+            return 0
+        flow = 0
+        for goto in input[curr]['goto']:
+            flow = max(flow, dfs(goto, time-1, seen))
+        if curr not in seen and input[curr]['flow'] > 0:
+            seen.add(curr)
+            flow += input[curr]['flow'] * (time-1)
+        
+        return flow
 
+    print(dfs('AA', 30, set()))
 
-    pass
 
 
 def partB(input):
@@ -26,15 +46,14 @@ def partB(input):
 
 
 def solveA(input):
-    valveMap, flowMap = input
-    return partA(valveMap, flowMap)
+    return partA(input)
 
 
 def solveB(input):
     return partB(input)
 
 
-# if __name__ == '__main__':
-#     input = parse(data)
-#     print(f'Part A: {partA(input)}')
-#     print(f'Part B: {partB(input)}')
+if __name__ == '__main__':
+    input = parse(test)
+    print(f'Part A: {partA(input)}')
+    # print(f'Part B: {partB(input)}')
