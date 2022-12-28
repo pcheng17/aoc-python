b8bc23de7d80c5a17da34ea03ea00a4a50672c73
Peter Cheng <pcheng17@gmail.com>
2022-12-28 00:09:31 -0800

    WIP 19

--- 2022/q19.py
+++ 2022/q19.py
@@ -18,138 +18,127 @@ def parse(data):
     return blueprints
 
 
-def collect(resources, robots):
-    for k, v in robots.items():
-        resources[k] += v
-    return resources
-
-def buyOreRobot(blueprint, resources, robots):
-    resources['ore'] -= blueprint[1]
-    robots['ore'] += 1
-    return (resources, robots)
-
-def buyClayRobot(blueprint, resources, robots):
-    resources['ore'] -= blueprint[2]
-    robots['clay'] += 1
-    return (resources, robots)
-
-def buyObsidianRobot(blueprint, resources, robots):
-    resources['ore'] -= blueprint[3]
-    resources['clay'] -= blueprint[4]
-    robots['obsidian'] += 1
-    return (resources, robots)
-
-def buyGeodeRobot(blueprint, resources, robots):
-    resources['ore'] -= blueprint[5]
-    resources['obsidian'] -= blueprint[6]
-    robots['geode'] += 1
-    return (resources, robots)
-
-
-def dfs(
-    blueprint,
-    resources,
-    robots,
-    max_geode_count,
-    time_remaining,
-    visited,
-    dp):
+def dfs(blueprint, ore_count, clay_count, obs_count, geode_count, ore_robot, clay_robot, obs_robot, geode_robot, max_geode_count, time_remaining, visited, dp):
 
     fset = frozenset(visited)
     dp[fset] = max(dp.get(fset, 0), max_geode_count)
 
     if time_remaining <= 0:
         return
-
-    print(f'Resources: {resources} ::: Robots: {robots}')
+    
+    print(ore_count, clay_count, obs_count, geode_count, ore_robot, clay_robot, obs_robot, geode_robot, max_geode_count, time_remaining)
 
     max_ore_cost = max(blueprint[1], blueprint[2], blueprint[3], blueprint[5])
 
-    if resources['ore'] >= blueprint[5] and resources['obsidian'] >= blueprint[6]: 
+    if ore_count >= blueprint[5] and obs_count >= blueprint[6]: 
         # Buy geode robot
-        resources = collect(resources, robots)
-        resources, robots = buyGeodeRobot(blueprint, resources, robots)
-        max_geode_count = resources['geode']
-        next_visited = visited.copy()
-        next_visited.add((
-            json.dumps(resources, sort_keys=True),
-            json.dumps(robots, sort_keys=True),
-            max_geode_count,
-            time_remaining - 1)) 
-        dfs(blueprint, resources, robots, max_geode_count, time_remaining - 1, next_visited, dp)
+        state = (
+            ore_count + ore_robot - blueprint[5],
+            clay_count + clay_robot,
+            obs_count + obs_robot - blueprint[6],
+            geode_count + geode_robot,
+            ore_robot,
+            clay_robot,
+            obs_robot,
+            geode_robot + 1,
+            max_geode_count + geode_robot,
+            time_remaining - 1
+        )
+        if state not in visited:
+            next_visited = visited.copy()
+            next_visited.add(state)
+            dfs(blueprint, *state, next_visited, dp)
 
-    elif robots['obsidian'] < blueprint[6] and resources['ore'] >= blueprint[3] and resources['clay'] >= blueprint[4]:
+    if obs_robot < blueprint[6] and ore_count >= blueprint[3] and clay_count >= blueprint[4]:
         # Buy obsidian robot
-        resources = collect(resources, robots)
-        resources, robots = buyObsidianRobot(blueprint, resources, robots)
-        max_geode_count = resources['geode']
-        next_visited = visited.copy()
-        next_visited.add((
-            json.dumps(resources, sort_keys=True),
-            json.dumps(robots, sort_keys=True),
-            max_geode_count,
-            time_remaining - 1)) 
-        dfs(blueprint, resources, robots, max_geode_count, time_remaining - 1, next_visited, dp)
+        state = (
+            ore_count + ore_robot - blueprint[3],
+            clay_count + clay_robot - blueprint[4],
+            obs_count + obs_robot,
+            geode_count + geode_robot,
+            ore_robot,
+            clay_robot,
+            obs_robot + 1,
+            geode_robot,
+            max_geode_count + geode_robot,
+            time_remaining - 1
+        )
+        if state not in visited:
+            next_visited = visited.copy()
+            next_visited.add(state)
+            dfs(blueprint, *state, next_visited, dp)
 
-    elif robots['clay'] < blueprint[4] and resources['ore'] >= blueprint[2]:
+    if clay_robot < blueprint[4] and ore_count >= blueprint[2]:
         # Buy clay robot
-        resources = collect(resources, robots)
-        resources, robots = buyClayRobot(blueprint, resources, robots)
-        max_geode_count = resources['geode']
-        next_visited = visited.copy()
-        next_visited.add((
-            json.dumps(resources, sort_keys=True),
-            json.dumps(robots, sort_keys=True),
-            max_geode_count,
-            time_remaining - 1)) 
-        dfs(blueprint, resources, robots, max_geode_count, time_remaining - 1, next_visited, dp)
+        state = (
+            ore_count + ore_robot - blueprint[2],
+            clay_count + clay_robot,
+            obs_count + obs_robot,
+            geode_count + geode_robot,
+            ore_robot,
+            clay_robot + 1,
+            obs_robot,
+            geode_robot,
+            max_geode_count + geode_robot,
+            time_remaining - 1
+        )
+        if state not in visited:
+            next_visited = visited.copy()
+            next_visited.add(state)
+            dfs(blueprint, *state, next_visited, dp)
 
-    elif robots['ore'] < max_ore_cost and resources['ore'] >= blueprint[1]:
+    if ore_robot < max_ore_cost and ore_count >= blueprint[1]:
         # Buy ore robot
-        resources = collect(resources, robots)
-        resources, robots = buyOreRobot(blueprint, resources, robots)
-        max_geode_count = resources['geode']
-        next_visited = visited.copy()
-        next_visited.add((
-            json.dumps(resources, sort_keys=True),
-            json.dumps(robots, sort_keys=True),
-            max_geode_count,
-            time_remaining - 1)) 
-        dfs(blueprint, resources, robots, max_geode_count, time_remaining - 1, next_visited, dp)
+        state = (
+            ore_count + ore_robot - blueprint[1],
+            clay_count + clay_robot,
+            obs_count + obs_robot,
+            geode_count + geode_robot,
+            ore_robot + 1,
+            clay_robot,
+            obs_robot,
+            geode_robot,
+            max_geode_count + geode_robot,
+            time_remaining - 1
+        )
+        if state not in visited:
+            next_visited = visited.copy()
+            next_visited.add(state)
+            dfs(blueprint, *state, next_visited, dp)
 
-    else:
-        # Do nothing
-        resources = collect(resources, robots)
-        max_geode_count = resources['geode']
+    # Do nothing
+    state = (
+        ore_count + ore_robot,
+        clay_count + clay_robot,
+        obs_count + obs_robot,
+        geode_count + geode_robot,
+        ore_robot,
+        clay_robot,
+        obs_robot,
+        geode_robot,
+        max_geode_count + geode_robot,
+        time_remaining - 1
+    )
+    if state not in visited:
         next_visited = visited.copy()
-        next_visited.add((
-            json.dumps(resources, sort_keys=True),
-            json.dumps(robots, sort_keys=True),
-            max_geode_count,
-            time_remaining - 1)) 
-        dfs(blueprint, resources, robots, max_geode_count, time_remaining - 1, next_visited, dp)
+        next_visited.add(state)
+        dfs(blueprint, *state, next_visited, dp)
 
 
 def simulate(blueprint, total_time) -> int:
 
-    resources = { 'ore': 0, 'clay': 0, 'obsidian': 0, 'geode': 0 }
-    robots    = { 'ore': 1, 'clay': 0, 'obsidian': 0, 'geode': 0 }
+    ore_count, clay_count, obs_count, geode_count = 0, 0, 0, 0
+    ore_robot, clay_robot, obs_robot, geode_robot = 1, 0, 0, 0
 
     dp, visited = {}, set()
     max_geode_count = 0
     visited.add((
-        resources['ore'], 
-        resources['clay'], 
-        resources['obsidian'], 
-        resources['geode'], 
-        robots['ore'], 
-        robots['clay'], 
-        robots['obsidian'], 
-        robots['geode'],
+        ore_count, clay_count, obs_count, geode_count,
+        ore_robot, clay_robot, obs_robot, geode_robot,
         max_geode_count,
         total_time)) 
-    dfs(blueprint, resources, robots, max_geode_count, total_time, visited, dp)
-    return max_geode_count
+    dfs(blueprint, ore_count, clay_count, obs_count, geode_count, ore_robot, clay_robot, obs_robot, geode_robot, max_geode_count, total_time, visited, dp)
+    return max(dp.values())
 
 
 def partA(blueprints):
