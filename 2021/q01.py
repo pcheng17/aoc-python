d068d45ecb60bda42441d47cb67c14d8dbe468ba
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2023-11-26 22:01:48 -0800

    Updates to previous years

--- 2021/q01.py
+++ 2021/q01.py
@@ -1,15 +1,13 @@
-from aocd import data
-
+from aocd import get_data
+raw_data = get_data(day=1, year=2021)
 
 def parse(data):
     return list(map(int, data.splitlines()))
 
-
-def partA(input):
+def part_a(input):
     return sum([b > a for a, b in zip(input, input[1:])])
 
-
-def partB(input):
+def part_b(input):
     windowSum = sum([input[i] for i in range(3)])
     count = 0
     for i in range(3, len(input)):
@@ -18,17 +16,3 @@ def partB(input):
             count += 1
         windowSum = tmp
     return count
-
-
-def solveA(input):
-    return partA(input)
-
-
-def solveB(input):
-    return partB(input)
-
-
-if __name__ == '__main__':
-    input = parse(data)
-    print(f'Part A: {solveA(input)}')
-    print(f'Part B: {solveB(input)}')
--- 2022/q01.py
+++ 2022/q01.py
@@ -1,5 +1,7 @@
-from aocd import data
+from aocd import get_data
 from common.utils import nlargest
+
+raw_data = get_data(day=1, year=2022)
 
 def parse(data):
     return data.split('\n\n')
@@ -7,19 +9,8 @@ def parse(data):
 def findTotalCalories(inputList):
     return [sum(map(int, line.split())) for line in inputList]
 
-def partA(inputList):
+def part_a(inputList):
     return max(findTotalCalories(inputList))
 
-def partB(inputList):
+def part_b(inputList):
     return sum(nlargest(3, findTotalCalories(inputList)))
-
-def solveA(input):
-    return partA(input)
-
-def solveB(input):
-    return partB(input)
-
-if __name__ == '__main__':
-    input = parse(data)
-    print(f"Part A: {partA(input)}")
-    print(f"Part B: {partB(input)}")
