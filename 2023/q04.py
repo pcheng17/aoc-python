7a98875fc37864467b7e3e1b4c6e2cfe4de2f869
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2023-12-03 21:52:53 -0800

    Update for correctness

--- 2023/q04.py
+++ 2023/q04.py
@@ -23,6 +23,7 @@ def part_a(input):
 
 def part_b(input):
     data = input.splitlines()
+    num_cards = len(data)
     cards = defaultdict(lambda: 0)
     for i, line in enumerate(data, 1):
         cards[i] += 1
@@ -31,6 +32,6 @@ def part_b(input):
         winning_cards = set(map(int, winning_cards_str.split()))
         my_cards = set(map(int, my_cards_str.split()))
         matches = my_cards.intersection(winning_cards)
-        for j in range(i + 1, i + len(matches) + 1):
+        for j in range(i + 1, min(i + len(matches) + 1, num_cards + 1)):
             cards[j] += cards[i]
     return sum(v for v in cards.values())
