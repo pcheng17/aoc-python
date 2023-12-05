a9778a91072ac4bfdccd95562e7e5b2530d3c6e3
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2023-12-04 23:27:41 -0800

    Finished 2023 05

---
+++ 2023/q05.py
@@ -0,0 +1,66 @@
+from aocd import get_data
+
+
+def get_input():
+    return get_data(day=5, year=2023)
+
+def check_for_overlap(m):
+    for i in range(0, len(m)-1):
+        if m[i][1] + m[i][2] - 1 >= m[i+1][1]:
+            print(m[i], m[i+1])
+
+def part_a(input):
+    a, b, c, d, e, f, g, h = input.split('\n\n')
+    seeds = [int(x) for x in a.split(':')[1].split(' ') if len(x) != 0]
+
+    map_data = [
+        [tuple(map(int, x.split(' '))) for x in b.splitlines()[1:]],
+        [tuple(map(int, x.split(' '))) for x in c.splitlines()[1:]],
+        [tuple(map(int, x.split(' '))) for x in d.splitlines()[1:]],
+        [tuple(map(int, x.split(' '))) for x in e.splitlines()[1:]],
+        [tuple(map(int, x.split(' '))) for x in f.splitlines()[1:]],
+        [tuple(map(int, x.split(' '))) for x in g.splitlines()[1:]],
+        [tuple(map(int, x.split(' '))) for x in h.splitlines()[1:]],
+    ]
+
+    for m in map_data:
+        new_seeds = []
+        for s in seeds:
+            found = False
+            for dst, src, rng in m:
+                if src <= s < src + rng:
+                    new_seeds.append(dst + (s - src))
+                    found = True
+                    break
+            if not found:
+                new_seeds.append(s)
+        seeds = new_seeds
+
+    return min(seeds)
+
+def part_b(input):
+    a, b, c, d, e, f, g, h = input.split('\n\n')
+    seeds = [int(x) for x in a.split(':')[1].split(' ') if len(x) != 0]
+    seed_rngs = [(seeds[i], seeds[i+1]) for i in range(0, len(seeds), 2)]
+    
+    map_data = [
+        [tuple(map(int, x.split(' '))) for x in b.splitlines()[1:]],
+        [tuple(map(int, x.split(' '))) for x in c.splitlines()[1:]],
+        [tuple(map(int, x.split(' '))) for x in d.splitlines()[1:]],
+        [tuple(map(int, x.split(' '))) for x in e.splitlines()[1:]],
+        [tuple(map(int, x.split(' '))) for x in f.splitlines()[1:]],
+        [tuple(map(int, x.split(' '))) for x in g.splitlines()[1:]],
+        [tuple(map(int, x.split(' '))) for x in h.splitlines()[1:]],
+    ]
+
+    for m in map_data:
+        new_seed_rngs = []    
+        for seed_start, seed_rng in seed_rngs:
+            for dst, src, rng in m:
+                if src <= seed_start < src + rng:
+                    new_seed_rngs.append((dst + (seed_start - src), min(seed_rng, (src + rng) - seed_start)))
+                elif seed_start < src < seed_start + seed_rng:
+                    new_seed_rngs.append((dst, min(rng, seed_rng - (src - seed_start))))
+        seed_rngs = new_seed_rngs
+    
+    return(min([x[0] for x in seed_rngs]))
