d96ce64462bc5c319a40546a9cdd4818cd1d685c
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2023-12-06 01:57:18 -0800

    Update 2020 01

--- 2020/01.py
+++ 2020/01.py
@@ -1,27 +1,26 @@
 def parse(input):
-    return sorted(list(map(int, input.splitlines())))
+    return list(map(int, input.splitlines()))
 
 def part_a(input):
-    data = parse(input)
-    i = 0
-    j = len(data) - 1
-    while i < j:
-        if data[i] + data[j] == 2020:
-            return data[i] * data[j]
-        elif data[i] + data[j] < 2020:
-            i += 1
-        else:
-            j -= 1
+    nums = parse(input)
+
+    seen = set()
+
+    for n in nums:
+        x = 2020 - n
+        if x in seen:
+            return n * x
+        seen.add(n)
 
 def part_b(input):
-    data = parse(input)
-    for i in range(len(data)):
+    nums = sorted(parse(input))
+    for i in range(len(nums)):
         j = i + 1
-        k = len(data) - 1
+        k = len(nums) - 1
         while j < k:
-            if data[i] + data[j] + data[k] == 2020:
-                return data[i] * data[j] * data[k]
-            elif data[i] + data[j] + data[k] < 2020:
+            if nums[i] + nums[j] + nums[k] == 2020:
+                return nums[i] * nums[j] * nums[k]
+            elif nums[i] + nums[j] + nums[k] < 2020:
                 j += 1
             else:
                 k -= 1
