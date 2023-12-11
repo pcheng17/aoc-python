b226890789a527187331c46f18c1f035b4aac96f
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2023-12-11 10:17:33 -0800

    Add to test

--- tests/test_grid2d.py
+++ tests/test_grid2d.py
@@ -37,3 +37,17 @@ def test_contruction():
     for i in range(3):
         for j in range(6):
             assert grid(i, j) == chr(6 * i + j + ord('a'))
+        
+@pytest.mark.parametrize(
+    'grid, delim, char, expected',  
+    [
+        '#..#\n..#.\n.#..', '\n', '#', [(0, 0), (0, 3), (1, 2), (2, 1)],
+        '... ... ...', ' ', '#', [],
+        '#,#,#,#\n#,#,#,#', '\n,', '#', [(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (1, 1), (1,  2), (1, 3)]
+    ]
+)
+def test_find_all(grid, delim, char, expected):
+    grid = Grid2D.from_str(grid, delim)
+    t = grid.find_all(char)
+    assert (all(x in t for x in expected) and all(x in expected for x in t))
+    
