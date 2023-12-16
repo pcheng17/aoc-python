412b141124caf948d698b8fd263568b501277236
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2023-12-15 23:43:42 -0800

    Clean up code

--- 2023/01.py
+++ 2023/01.py
@@ -1,32 +1,22 @@
-replace_dict = {
-    'one':   'o1e',
-    'two':   't2o',
-    'three': 't3e',
-    'four':  'f4r',
-    'five':  'f5e',
-    'six':   's6x',
-    'seven': 's7n',
-    'eight': 'e8t',
-    'nine':  'n9e'   
-}
-
 def replace_numbers(string):
-    for k, v in replace_dict.items():
+    m = {
+        'one':   'o1e',
+        'two':   't2o',
+        'three': 't3e',
+        'four':  'f4r',
+        'five':  'f5e',
+        'six':   's6x',
+        'seven': 's7n',
+        'eight': 'e8t',
+        'nine':  'n9e'   
+    }
+    for k, v in m.items():
         string = string.replace(k, v)
     return string
 
 def find_number(string):
-    for c in string:
-        if c.isdigit():
-            first = c
-            break    
-
-    for c in string[::-1]:
-        if c.isdigit():
-            last = c
-            break
-    
-    return int(first + last)
+    d = [c for c in string if c.isdigit()]
+    return int(d[0] + d[-1])
 
 def part_a(input):
     return sum([find_number(line) for line in input.splitlines()])
