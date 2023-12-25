2307e405f8664b6bb0c82d0f1886656e149d090d
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2023-12-24 16:21:14 -0800

    Working on 17

--- 2023/17.py
+++ 2023/17.py
@@ -13,49 +13,55 @@ def solve(grid, queue):
     visited = {}
 
     while queue:
-        i, j, d, sd, hl = heappop(queue)
+        hl, (i, j), d, sd, visited = heappop(queue)
+
+        if not in_bounds(grid, i, j):
+            continue
+
+        if (i, j) in visited:
+            continue
+
+        visited.add((i, j))
 
         heat_loss = int(grid[i][j])
 
-        # We've reached the end
         if i == rows - 1 and j == cols - 1:
             print(f'Found min heat loss: {hl + heat_loss}')
+            print(f'Visited: {visited}')
             min_heat_loss = min(min_heat_loss, hl + heat_loss)
             continue
 
-
-        # if (i, j) in visited and min_heat_loss < visited[(i, j)]:
-        #     continue
-
-        # visited[(i, j, d, sd)] = hl
+        if hl + heat_loss >= min_heat_loss:
+            continue
 
         if d == '>':
             if sd < 3:
-                tmp.append((i, j + 1, '>', sd + 1, hl + heat_loss))
-            tmp.append((i + 1, j, 'V', 0, hl + heat_loss))
-            tmp.append((i - 1, j, '^', 0, hl + heat_loss))
+                heappush(queue, (hl + heat_loss, (i, j + 1), '>', sd + 1, visited.copy()))
+            heappush(queue, (hl + heat_loss, (i - 1, j), '^', 0, visited.copy()))
+            heappush(queue, (hl + heat_loss, (i + 1, j), 'v', 0, visited.copy()))
         elif dir == '^':
             if sd < 3:
-                tmp.append((i - 1, j, '^', sd + 1, hl + heat_loss))
-            tmp.append((i, j - 1, '<', 0, hl + heat_loss))
-            tmp.append((i, j + 1, '>', 0, hl + heat_loss))
+                heappush(queue, (hl + heat_loss, (i - 1, j), '^', sd + 1, visited.copy()))
+            heappush(queue, (hl + heat_loss, (i, j + 1), '>', 0, visited.copy()))
+            heappush(queue, (hl + heat_loss, (i, j - 1), '<', 0, visited.copy()))
         elif dir == '<':
             if sd < 3:
-                tmp.append((i, j - 1, '<', sd + 1, hl + heat_loss))
-            tmp.append((i + 1, j, 'V', 0, hl + heat_loss))
-            tmp.append((i - 1, j, '^', 0, hl + heat_loss))
-        else: # dir == 'V'
+                heappush(queue, (hl + heat_loss, (i, j - 1), '<', sd + 1, visited.copy()))
+            heappush(queue, (hl + heat_loss, (i - 1, j), '^', 0, visited.copy()))
+            heappush(queue, (hl + heat_loss, (i + 1, j), 'v', 0, visited.copy()))
+        else: # dir == 'v'
             if sd < 3:
-                tmp.append((i + 1, j, 'V', sd + 1, hl + heat_loss))
-            tmp.append((i, j - 1, '<', 0, hl + heat_loss))
-            tmp.append((i, j + 1, '>', 0, hl + heat_loss))
+                heappush(queue, (hl + heat_loss, (i + 1, j), 'v', sd + 1, visited.copy()))
+            heappush(queue, (hl + heat_loss, (i, j + 1), '>', 0, visited.copy()))
+            heappush(queue, (hl + heat_loss, (i, j - 1), '<', 0, visited.copy()))
+
+    return min_heat_loss
 
 def part_a(input):
     data = tuple(input.splitlines())
-    # (i, j, dir, same_dir_count, heat_loss)
+    # (heat_loss, (i, j), dir, same_dir_count)
     queue = [
-        (0, 0, '>', 0, -int(data[0][0])),
-        (0, 0, 'V', 0, -int(data[0][0]))
+        (-int(data[0][0]), (0, 0), '>', -1, set()),
     ]
     heapify(queue)
     return solve(data, queue)
