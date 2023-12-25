2124fcea3ec40b4689093e3ddf84b6ea8d15c15b
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2023-12-24 17:07:50 -0800

    WIP: Dijsktra

--- 2023/17.py
+++ 2023/17.py
@@ -1,70 +1,69 @@
 import math
 from heapq import heapify, heappush, heappop
+from collections import defaultdict
 
 def in_bounds(grid, i, j):
     return 0 <= i < len(grid) and 0 <= j < len(grid[0])
 
-def solve(grid, queue):
+def dijkstra(grid, start):
     rows = len(grid)
     cols = len(grid[0])
 
-    min_heat_loss = math.inf
+    visited = {start}
+    heat_loss = defaultdict(lambda: math.inf)
+    heat_loss[start] = 0
 
-    visited = {}
+    # current heat loss, position, direction, same direction count
+    queue = [
+        (int(grid[0][1]), (0, 1), '>', 1),
+        (int(grid[1][0]), (1, 0), 'v', 1),
+    ]
 
     while queue:
-        hl, (i, j), d, sd, visited = heappop(queue)
-
-        if not in_bounds(grid, i, j):
-            continue
+        hl, (i, j), d, sd = heappop(queue)
 
         if (i, j) in visited:
             continue
 
         visited.add((i, j))
-
-        heat_loss = int(grid[i][j])
-
-        if i == rows - 1 and j == cols - 1:
-            print(f'Found min heat loss: {hl + heat_loss}')
-            print(f'Visited: {visited}')
-            min_heat_loss = min(min_heat_loss, hl + heat_loss)
-            continue
-
-        if hl + heat_loss >= min_heat_loss:
-            continue
+        heat_loss[(i, j)] = min(heat_loss[(i, j)], hl)
 
         if d == '>':
-            if sd < 3:
-                heappush(queue, (hl + heat_loss, (i, j + 1), '>', sd + 1, visited.copy()))
-            heappush(queue, (hl + heat_loss, (i - 1, j), '^', 0, visited.copy()))
-            heappush(queue, (hl + heat_loss, (i + 1, j), 'v', 0, visited.copy()))
-        elif dir == '^':
-            if sd < 3:
-                heappush(queue, (hl + heat_loss, (i - 1, j), '^', sd + 1, visited.copy()))
-            heappush(queue, (hl + heat_loss, (i, j + 1), '>', 0, visited.copy()))
-            heappush(queue, (hl + heat_loss, (i, j - 1), '<', 0, visited.copy()))
-        elif dir == '<':
-            if sd < 3:
-                heappush(queue, (hl + heat_loss, (i, j - 1), '<', sd + 1, visited.copy()))
-            heappush(queue, (hl + heat_loss, (i - 1, j), '^', 0, visited.copy()))
-            heappush(queue, (hl + heat_loss, (i + 1, j), 'v', 0, visited.copy()))
-        else: # dir == 'v'
-            if sd < 3:
-                heappush(queue, (hl + heat_loss, (i + 1, j), 'v', sd + 1, visited.copy()))
-            heappush(queue, (hl + heat_loss, (i, j + 1), '>', 0, visited.copy()))
-            heappush(queue, (hl + heat_loss, (i, j - 1), '<', 0, visited.copy()))
+            if sd < 3 and in_bounds(grid, i, j + 1):
+                heappush(queue, (hl + int(grid[i][j + 1]), (i, j + 1), '>', sd + 1))
+            if in_bounds(grid, i - 1, j):
+                heappush(queue, (hl + int(grid[i - 1][j]), (i - 1, j), '^', 0))
+            if in_bounds(grid, i + 1, j):
+                heappush(queue, (hl + int(grid[i + 1][j]), (i + 1, j), 'v', 0))
+        elif d == '^':
+            if sd < 3 and in_bounds(grid, i - 1, j):
+                heappush(queue, (hl + int(grid[i - 1][j]), (i - 1, j), '^', sd + 1))
+            if in_bounds(grid, i, j + 1):
+                heappush(queue, (hl + int(grid[i][j + 1]), (i, j + 1), '>', 0))
+            if in_bounds(grid, i, j - 1):
+                heappush(queue, (hl + int(grid[i][j - 1]), (i, j - 1), '<', 0))
+        elif d == '<':
+            if sd < 3 and in_bounds(grid, i, j - 1):
+                heappush(queue, (hl + int(grid[i][j - 1]), (i, j - 1), '<', sd + 1))
+            if in_bounds(grid, i - 1, j):
+                heappush(queue, (hl + int(grid[i - 1][j]), (i - 1, j), '^', 0))
+            if in_bounds(grid, i + 1, j):
+                heappush(queue, (hl + int(grid[i + 1][j]), (i + 1, j), 'v', 0))
+        elif d == 'v':
+            if sd < 3 and in_bounds(grid, i + 1, j):
+                heappush(queue, (hl + int(grid[i + 1][j]), (i + 1, j), 'v', sd + 1))
+            if in_bounds(grid, i, j + 1):
+                heappush(queue, (hl + int(grid[i][j + 1]), (i, j + 1), '>', 0))
+            if in_bounds(grid, i, j - 1):
+                heappush(queue, (hl + int(grid[i][j - 1]), (i, j - 1), '<', 0))
 
-    return min_heat_loss
+    return heat_loss[(rows - 1, cols - 1)]
 
 def part_a(input):
     data = tuple(input.splitlines())
-    # (heat_loss, (i, j), dir, same_dir_count)
-    queue = [
-        (-int(data[0][0]), (0, 0), '>', -1, set()),
-    ]
-    heapify(queue)
-    return solve(data, queue)
+    return dijkstra(data, (0, 0))
 
 def part_b(input):
     data = input.splitlines()
+
+# 662 -- too low
