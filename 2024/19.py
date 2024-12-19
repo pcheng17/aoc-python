d885d3d2bd35d4e9106f2182f174af88837e7a2b
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-12-18 23:20:45 -0800

    2024 19

--- 2024/19.py
+++ 2024/19.py
@@ -1,52 +1,34 @@
-import functools
-
-@functools.lru_cache(maxsize=None)
-def dfs(word, subwords, start_pos = 0):
-    if start_pos == len(word):
-        return True
-
-    for subword in subwords:
-        if word[start_pos:].startswith(subword):
-            if dfs(word, subwords, start_pos + len(subword)):
-                return True
-
-    return False
-
-def dfs_find_all(word, subwords, start_pos, path = []):
-    if start_pos == len(word):
-        return path.copy()
-
-    all_paths = []
-
-    for subword in subwords:
-        if word[start_pos:].startswith(subword):
-            path.append(subword)
-            tmp = dfs_find_all(word, subwords, start_pos + len(subword), path)
-            all_paths.extend(tmp)
-            path.pop()
-
-    return all_paths
-
 def part_a(input):
-    # with open('./2024/bonsoon19.txt') as f:
-    #     input = f.read()
     a, b = input.split("\n\n")
-    subwords = tuple(a.split(', '))
-    words = b.split('\n')
-    total = 0
-    for w in words:
-        if dfs(w, subwords):
-            total += 1
-    return total
+    subwords, words = tuple(a.split(', ')), b.split('\n')
+
+    seen = set()
+    def dfs(w):
+        if len(w) == 0 or w in seen:
+            return True
+        for sw in subwords:
+            if w.startswith(sw) and dfs(w[len(sw):]):
+                seen.add(w)
+                return True
+        return False
+
+    return sum([dfs(w) for w in words])
 
 def part_b(input):
     a, b = input.split("\n\n")
-    subwords = a.split(', ')
-    words = b.split('\n')
-    total = 0
-    # for w in words:
-    #     if dfs(w, subwords):
-    #         tmp = dfs_find_all(w, subwords, 0)
-    #         tmp = ''.join(tmp)
-    #         total += tmp.count(w)
-    return total
+    subwords, words = a.split(', '), b.split('\n')
+
+    seen = {}
+    def find_all(w):
+        if len(w) == 0:
+            return 1
+        if w in seen:
+            return seen[w]
+        count = 0
+        for sw in subwords:
+            if w.startswith(sw):
+                count += find_all(w[len(sw):])
+        seen[w] = count
+        return count
+
+    return sum([find_all(w) for w in words])
