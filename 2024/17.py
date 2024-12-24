79eccc0ee10aef1273949a69ddc8227f0ce6dbe6
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-12-24 15:15:56 -0800

    2024 17

--- 2024/17.py
+++ 2024/17.py
@@ -1,5 +1,4 @@
-import itertools
-
+import sys
 class Machine:
     def __init__(self, a, b, c, program):
         self.a = a
@@ -21,15 +20,12 @@ class Machine:
     def run(self):
         self.output = []
         i = 0
-        print(f"A={self.a}, B={self.b}, C={self.c}, Output={self.output}")
         while i < len(self.program):
             result = self.ops[self.program[i]](self.program[i + 1])
             if result is None:
                 i += 2
             else:
                 i = result
-
-            print(f"A={self.a}, B={self.b}, C={self.c}, Output={self.output}")
 
     def combo(self, n):
         if n < 0 or n > 7:
@@ -80,7 +76,7 @@ def part_a(input):
 
     machine = Machine(A, B, C, program)
     machine.run()
-    # return ','.join(map(str, machine.output))
+    return ','.join(map(str, machine.output))
 
 def part_b(input):
     registers, program = input.split('\n\n')
@@ -91,6 +87,35 @@ def part_b(input):
     C = int(rows[2].split(': ')[1])
     program = list(map(int, program.split(': ')[1].split(',')))
 
+    constraints = []
+    for n in range(8):
+        tmp = []
+        for b in range(8):
+            c1 = b ^ 2 ^ 3          # Bottom 3 bits
+            m1 = 7                  # Mask of bottom 3 bits
+            c2 = (b ^ n) << (b ^ 3) # Other 3 bits
+            m2 = 7 << (b ^ 3)       # Mask of other 3 bits
+            m = m1 & m2             # Intersection of masks
+            if c1 & m == c2 & m:
+                tmp.append((c1 | c2, m1 | m2)) # Merge the two numbers
+        constraints.append(tmp)
+
+    candidates = set(constraints[program[0]])
+    for i in range(len(program)):
+        goal = program[i]
+        z = set((c << (3 * i), m << (3 * i)) for c, m in constraints[goal])
+        new_candidates = set()
+
+        for (constraint, mask_constraint) in z:
+            for (candidate, mask_candidate) in candidates:
+                m = mask_candidate & mask_constraint
+                if (candidate & m) == (constraint & m):
+                    new_candidates.add((candidate | constraint, mask_candidate | mask_constraint))
+
+        candidates = new_candidates
+
+    A = min(candidates, key=lambda x: x[0])[0]
     machine = Machine(A, B, C, program)
     machine.run()
-    return ','.join(map(str, machine.output))
+
+    return min(candidates, key=lambda x: x[0])[0]
