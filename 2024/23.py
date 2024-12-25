a60d0b4ef7455fb0520f587c73a10dbe0cd25f46
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-12-25 13:39:36 -0800

    fix

--- 2024/23.py
+++ 2024/23.py
@@ -1,12 +1,9 @@
 from collections import defaultdict
-from graphviz import Digraph
 
 def part_a(input):
     graph = defaultdict(set)
-    dot = Digraph()
     for row in input.splitlines():
         a, b = row.split("-")
-        dot.edge(a, b)
         graph[a].add(b)
         graph[b].add(a)
 
@@ -18,7 +15,6 @@ def part_a(input):
                 if b != c and c in graph[b]:
                     triangles.add(tuple(sorted([a, b, c])))
 
-    print(f"Found {len(triangles)} triangles")
     return len([tri for tri in triangles if any(x for x in tri if x.startswith('t'))])
 
 def part_b(input):
@@ -36,55 +32,34 @@ def part_b(input):
                 if b != c and c in graph[b]:
                     triangles.add(tuple(sorted([a, b, c])))
 
-    nodes = set()
-    for t in triangles:
-        for v in t:
-            nodes.add(v)
+    # Find the largest complete subgraph
+    nodes = set(graph.keys())
+    curr_best = set()
 
-    a = {v: len([t for t in triangles if v in t]) for v in nodes}
+    while nodes:
+        degs = {node: len(graph[node]) for node in nodes}
+        curr = max(degs.items(), key=lambda x: x[1])[0]
+        subgraph = {curr}
+        candidates = set(graph[curr])
+        while candidates:
+            valid_candidates = {
+                v : len(graph[v].intersection(subgraph))
+                for v in candidates
+                if all(u in graph[v] for u in subgraph)
+            }
 
-    b = defaultdict(set)
-    for k, v in a.items():
-        b[v].add(k)
+            if not valid_candidates:
+                break
 
-    for k, v in b.items():
-        print(f"{k}: {v}")
+            curr = max(valid_candidates.items(), key=lambda x: x[1])[0]
+            subgraph.add(curr)
 
+            candidates = candidates.intersection(graph[curr])
 
+        if curr_best is None or len(subgraph) > len(curr_best):
+            curr_best = subgraph
 
+        nodes -= subgraph
 
-
-
-
-
-    # # Find the largest complete subgraph
-    # nodes = set(graph.keys())
-    # curr_best = set()
-    #
-    # while nodes:
-    #     degs = {node: len(graph[node]) for node in nodes}
-    #     curr = max(degs.items(), key=lambda x: x[1])[0]
-    #     subgraph = {curr}
-    #     candidates = set(graph[curr])
-    #     while candidates:
-    #         valid_candidates = {
-    #             v : len(graph[v].intersection(subgraph))
-    #             for v in candidates
-    #             if all(u in graph[v] for u in subgraph)
-    #         }
-    #
-    #         if not valid_candidates:
-    #             break
-    #
-    #         curr = max(valid_candidates.items(), key=lambda x: x[1])[0]
-    #         subgraph.add(curr)
-    #
-    #         candidates = candidates.intersection(graph[curr])
-    #
-    #     if curr_best is None or len(subgraph) > len(curr_best):
-    #         curr_best = subgraph
-    #
-    #     nodes -= subgraph
-    #
-    # return ','.join(sorted(curr_best))
+    return ','.join(sorted(curr_best))
 
